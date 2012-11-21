4cef94e7eba02d44359e22c3f115d710769465cb
Ivan Calderon <iwano@hotmail.com>
2012-11-21 15:08:10 -0600

    Photos from especific albumx5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,7 +21,7 @@ class HomeController < ActionController::Base
 		  
 		@api = Koala::Facebook::API.new(session[:access_token])
 		begin
-			@graph_data = @api.get_connections("1424188961843", "fields"=>"photo")
+			@graph_data = @api.get_connections("1424188961843", "photos")
 		rescue Exception=>ex
 			puts ex.message
 		end
