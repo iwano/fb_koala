32b1b0f5062f8904abbab6282d17bf6bba97777a
Ivan Calderon <iwano@hotmail.com>
2012-11-21 17:58:30 -0600

    Fetch photos from crowds fan page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,37 +1,9 @@
 class HomeController < ActionController::Base
   protect_from_forgery
-  
-   def index   
-   	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, 'http://fb-koala-sample.herokuapp.com/home/callback')
-		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"user_photos") 	
-		puts session.to_s + "<<< session"
 
-  	respond_to do |format|
-			 format.html {  }
-		 end
-  end
-
-	def callback
-  	if params[:code]
-  		# acknowledge code and get access token from FB
-		  session[:access_token] = session[:oauth].get_access_token(params[:code])
-		end		
-
-		 # auth established, now do a graph call:
-		  
-		@api = Koala::Facebook::API.new(session[:access_token])
-		begin
-			@graph_data = @api.get_connections("1424188961843", "photos")
-		rescue Exception=>ex
-			puts ex.message
-		end
-		
-  
- 		respond_to do |format|
-		 format.html {   }			 
-		end
-		
-	
+	def index
+		@api = Koala::Facebook::API.new
+		@graph_data = @api.get_connections("10151326486988829", "photos")
 	end
 end
 
